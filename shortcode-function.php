8e0ed99921cc4cc5e683fb3c1e5d03478045ee3f
DipuHaider <dipuhaider@gmail.com>
2019-02-02 03:59:08 +0600

    updated shortcode-function.php
    code to show list of movies are included.

--- shortcode-function.php
+++ shortcode-function.php
@@ -1,11 +1,30 @@
 <?php 
 
-add_shortcode( 'helloworld', 'torque_hello_world_shortcode' );
+add_shortcode( 'movielist', 'list_of_movie_shortcode' );
 
-function torque_hello_world_shortcode( $atts ) {
-   $a = shortcode_atts( array(
-      'name' => 'world'
-   ), $atts );
-   return 'Hello ' . $a['name'] . '!';
+function list_of_movie_shortcode() {
+	
+// the query
+$wpb_all_query = new WP_Query(array('post_type'=>'films', 'post_status'=>'publish', 'posts_per_page'=>-1));?>
+ 
+<?php if ( $wpb_all_query->have_posts() ) : ?>
+ 
+<ul>
+ 
+    <!-- the loop -->
+    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+ 
+</ul>
+ 
+    <?php wp_reset_postdata(); ?>
+ 
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif;
 }
-?>
+?>		
+
+	
